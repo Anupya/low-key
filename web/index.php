64ef8b45adadddbb6682a6e7f42f89bcb531edd6
Anupya Pamidimukkala <anupya@hotmail.ca>
2018-03-03 17:48:22 -0500

    browse

--- web/index.php
+++ web/index.php
@@ -22,6 +22,8 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+/* ADD BROWSE PAGE */
+
 $app->get('/browse', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('browse.twig');
